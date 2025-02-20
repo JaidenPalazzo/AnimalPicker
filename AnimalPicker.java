23bd2281e869a504328abcd1a0798d5d53d9a9de
JaidenPalazzo <jaiden.palazzo@quinnipiac.edu>
2025-02-19 19:50:46 -0500

    ask if user wants to see cat or dog

---
+++ AnimalPicker.java
@@ -0,0 +1,29 @@
+import java.util.Scanner; 
+
+public class AnimalPicker{
+
+    private String animal; 
+    Scanner scanner = new Scanner(System.in); 
+    
+    public AnimalPicker(){
+        System.out.println("Which animal would yu like to see? (cat/dog)"); 
+        animal = scanner.nextLine(); 
+
+        if (animal.equals("cat")){
+            System.out.println("CAT ASCII ART HERE"); 
+        }
+
+        else if(animal.equals("dog")) {
+            System.out.println("DOG ASCII ART HERE");
+        }
+
+        else {
+            System.out.println("Invalid choice!"); 
+        }
+    }
+
+    public static void main(String [] args){
+        new AnimalPicker(); 
+    }
+
+}
